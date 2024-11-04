a8e886bb091bb7f171a3a24eac132c0b920af987
POTATOPROGAMMER <codercode1010@gmail.com>
2024-11-04 18:11:20 +0400

    commit2

---
+++ activity 1.py
@@ -0,0 +1,12 @@
+print ("enter the marks")
+math = int(input("maths :"))
+
+english = int(input("english :"))
+
+science = int(input("science :"))
+
+x=science+math+english
+print (x)
+35
+percent=(x/300)*100
+print (percent)
